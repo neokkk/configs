7ae9c7a0928514885127f54d16a0e7ebd08c9d63
neokkk <seven3126@gmail.com>
2024-06-15 13:38:33 +0900

    feat: setup

--- dotfiles/nvim/setup.sh
+++ dotfiles/nvim/setup.sh
@@ -2,4 +2,27 @@
 
 sudo apt update
 
-sudo apt install ripgrep
+# Install Dependencies for Telescope
+
+apt list --installed ripgrep | grep installed
+
+if [ $? -ne 0 ]; then
+    sudo apt install ripgrep
+fi
+
+# Install Github Copilot
+
+node --version
+
+if [ $? -ne 0 ]; then
+    sudo apt install nvm
+    nvm install -lts
+    node --version
+    if [ $? -ne 0 ]; then
+        echo "Unable to install Node.js"
+        exit 1
+    fi
+fi
+
+git clone https://github.com/github/copilot.vim.git \
+    ~/.config/nvim/pack/github/start/copilot.vim
