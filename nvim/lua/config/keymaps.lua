995aa415eb3ae98d9185e723934c112abc2c692e
neokkk <seven3126@gmail.com>
2024-07-29 13:28:09 +0900

    feat

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,10 +5,14 @@ local mapKey = require("utils/keyMapper").mapKey
 mapKey("<leader>e", ":Neotree position=left dir=%:p:h:h toggle<cr>")
 
 -- navigate pane
-mapKey("<C-h>", "<C-w>h") -- left
-mapKey("<C-j>", "<C-w>j") -- down
-mapKey("<C-k>", "<C-w>k") -- up
-mapKey("<C-l>", "<C-w>l") -- right
+mapKey("<C-h>", "<C-w>h")  -- left
+mapKey("<C-j>", "<C-w>j")  -- down
+mapKey("<C-k>", "<C-w>k")  -- up
+mapKey("<C-l>", "<C-w>l")  -- right
+
+mapKey('<C-n>', ':bp<CR>') -- priv
+mapKey('<C-m>', ':bn<CR>') -- next
+mapKey('<C-q>', ':bd<CR>') -- close
 
 -- Neotree end
 
