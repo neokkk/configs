97d22b1fe1dc93985961a6623079cac4258d056f
neokkk <seven3126@gmail.com>
2025-01-10 13:13:27 +0900

    feat: disable autocmd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,7 +19,11 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.clangd.setup({})
+            lspconfig.clangd.setup({
+                on_attach = function(client)
+                    client.server_capabilities.documentFormattingProvider = false
+                end
+            })
             lspconfig.lua_ls.setup({})
             lspconfig.pyright.setup({})
             lspconfig.tsserver.setup({})
