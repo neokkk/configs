674240f2637dd636c9bb7658da4f76de7cd7361f
Nakyeong Kim <35194760+neokkk@users.noreply.github.com>
2025-02-28 13:31:29 +0900

    Update setup.sh

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -11,7 +11,7 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 echo "
-export PATH=\$PATH:/opt/nvim-linux64/bin
+export PATH=\$PATH:/opt/nvim-linux-x86_64/bin
 alias nv=\"nvim\"" >> ~/.bashrc
 source ~/.bashrc
 
