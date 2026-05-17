f3cd44babee9e41efa8f8db99ee2e16188ed9f15
Nakyeong Kim <35194760+neokkk@users.noreply.github.com>
2026-05-17 17:45:42 +0900

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,36 +1,55 @@
 local mapKey = require("utils/keyMapper").mapKey
 
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "clangd", "lua_ls", "pyright", "tsserver" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.clangd.setup({
-                on_attach = function(client)
-                    client.server_capabilities.documentFormattingProvider = false
-                end
-            })
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.tsserver.setup({})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "clangd",
+          "lua_ls",
+          "pyright",
+          "ts_ls",
+          "rust_analyzer",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp = vim.lsp
 
-            mapKey("K", vim.lsp.buf.hover)
-            mapKey("gd", vim.lsp.buf.definition)
-            mapKey("<leader>ca", vim.lsp.buf.code_action)
-        end
-    },
+      lsp.config("clangd", {
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+      })
+
+      lsp.config("lua_ls", {})
+      lsp.config("pyright", {})
+      lsp.config("ts_ls", {})
+
+      lsp.enable("clangd")
+      lsp.enable("lua_ls")
+      lsp.enable("pyright")
+      lsp.enable("ts_ls")
+      -- rust_analyzer는 rustaceanvim이 설정하므로 여기서 setup하지 않음
+
+      mapKey("K", lsp.buf.hover)
+      mapKey("gd", lsp.buf.definition)
+      mapKey("<leader>ca", lsp.buf.code_action)
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^6",
+    lazy = false,
+  },
 }
