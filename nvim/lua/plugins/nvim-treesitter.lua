d4b18a3cb756ce13b83a71f353117bcdcabd3bfd
Nakyeong Kim <35194760+neokkk@users.noreply.github.com>
2026-05-19 14:45:07 +0900

    Update nvim-treesitter.lua

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-      require("nvim-treesitter").install { "c", "cpp", "javascript", "lua", "python", "rust" }
+        require("nvim-treesitter").install { "c", "cpp", "javascript", "lua", "python", "rust" }
     end
 }
